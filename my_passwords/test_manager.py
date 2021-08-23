565151f8e4012bad8a3ee14b48e3c9eb8907e8b1
toorajjahangiri <toorajjahangiri@gmail.com>
2021-08-23 17:15:29 +0430

    add test account update

--- my_passwords/test_manager.py
+++ my_passwords/test_manager.py
@@ -2,7 +2,7 @@
 __author__ = "ToorajJahangiri"
 __email__ = "Toorajjahangiri@gmail.com"
 ###########################################
-from manager import crypto, key_maker, Cipher, Manager
+from manager import crypto, key_maker, Cipher, Manager, account_update
 
 # TEST CRYPTO
 def test_crypting(inp: str, key: str) -> bool:
@@ -61,6 +61,23 @@ def test_manager(user:str, password: str) -> bool:
         print(f"{AssertionError}")
         return False
 
+# TEST ACCOUNT UPDATE
+def test_account_update(user: str, password: str, new_user: str, new_password: str, db_size: int = 30, path: str = None) -> bool:
+    path = './test/t.db' if path is None else path
+    def make() -> bool:
+        m_val = ((f'Name_{i}', f'Password_{i}', f'URL_{i}', f'More_{i}') for i in range(0, db_size))
+        db = Manager(path, user, password)
+        gen = (db.add_pass(*i) for i in m_val)
+        tmp = []
+        for i in gen:
+            tmp.append(i)
+        return all(tmp)
+    mk_db = make()
+    if mk_db is True:
+        return account_update(path, user, password, new_user, new_password)[0]
+    else:
+        return False
+
 if __name__ == "__main__":
     print(f"{'':>5}-> START TESTS <-", end='\n\n')
 
@@ -72,5 +89,7 @@ if __name__ == "__main__":
     print(f"{'':>5}Cipher :: {t_ci}", end='\n\n')
     t_ma = test_manager('test', '*TEST*')
     print(f"{'':>5}Manager :: {t_ma}", end='\n\n')
+    t_au = test_account_update('test', '*TEST*', 'NEW_USER', '**NEW_PASSWORD**', 60, None)
+    print(f"{'':>5}AccountUpdate :: {t_au}", end='\n\n')
 
     print(f"{'':>5}->    DONE    <-", end='\n')
